6ec4bd7f454ebf0bbda50160e1cb55ec36530e2e
Ogawa <135859092+ogawa3427@users.noreply.github.com>
2025-05-28 20:35:53 +0900

    デコと投稿ボタ（もう一度）

--- scripts/version-up.sh
+++ scripts/version-up.sh
@@ -10,14 +10,16 @@ get_current_version() {
 if [ $# -eq 0 ]; then
     get_current_version
     exit 0
-elif [ $# -ne 1 ]; then
-    echo "使用方法: $0 [バージョン]"
-    echo "例: $0 0.0.2"
+elif [ $# -lt 1 ] || [ $# -gt 2 ]; then
+    echo "使用方法: $0 [バージョン] [コミットメッセージ]"
+    echo "例: $0 0.0.2 \"新機能追加\""
     echo "バージョンを指定しない場合は現在のバージョンが表示されます"
+    echo "コミットメッセージを省略した場合はデフォルトメッセージが使用されます"
     exit 1
 fi
 
 NEW_VERSION=$1
+COMMIT_MESSAGE=${2:-"chore: bump version to $NEW_VERSION"}
 
 # package.jsonの更新
 sed -i '' "s/\"version\": \".*\"/\"version\": \"$NEW_VERSION\"/" package.json
@@ -26,8 +28,9 @@ sed -i '' "s/\"version\": \".*\"/\"version\": \"$NEW_VERSION\"/" package.json
 sed -i '' "s/\"version\": \".*\"/\"version\": \"$NEW_VERSION\"/" app/manifest.json
 
 # 変更をコミット
+git add -A
 git add package.json app/manifest.json
-git commit -m "chore: bump version to $NEW_VERSION"
+git commit -m "$COMMIT_MESSAGE"
 
 # タグの作成とプッシュ
 git tag -a "v$NEW_VERSION" -m "Release version $NEW_VERSION"
